2720f75aac92c2faaa3df0b4ba6ea729d6524668
Sangeetakumari <106247119+SHARMILAGUPTA123@users.noreply.github.com>
2023-05-30 12:40:32 +0530

    Add files via upload

---
+++ nestedlist.py
@@ -0,0 +1,401 @@
+
+# b=[]
+# i=0
+# while i<len(a):
+#    j=0
+#    while j<len(i):
+#     if a[i][j] in a:
+#       b.
+# a=[1,2,3,[4,5,6],7,8,9,10]
+# b=[]
+# for i in range(len(a)):
+#         if i==3:
+#           d=list(a[i])
+#           for j in d:
+#                 b.append(j)
+#         else:
+#               b.append(a[i])
+         
+# print(b)
+# a=[1,2,3,[5,6],[7,8],9,10]
+# b=[]
+# i=0# a=[[1,3,4],[2,4],[3],[5,6],[5]]
+
+# while i<=len(a):
+
+#     for i in a:
+#         if a[i]==4:
+#             s=int(a[i])
+#             for j in s:
+#                 b.append(j)
+#         else:
+#             b.append(a[i])
+#     i+=1
+#     print(b)
+# lis=[]
+# i=0
+# while i<=4:
+#     tem=[]
+#     j=0
+#     while  j<(i+1):
+#       if j==0 or j==i:
+#         tem.append(1)
+#       else:
+#          tem.append(lis[i-1][j-1]+lis[i-1][j])  
+#       j+=1
+#     lis.append(tem)    
+#     i+=1
+# print(lis)
+# i=0
+# while i<len(lis):
+#     j=0
+#     while j<=i:
+#       print(lis[j],end="")
+      # j+=1
+    # print()  
+    # i+=1
+
+# i=1
+# fac=0
+# l=[]
+# while i<=4:
+#     j=1
+#     lis=[]
+#     while j<=i:
+#       if i%j==0:
+#         lis.append(j)
+#       j+=1
+#     l.append(lis)  
+#     i+=1
+# print(l)    
+# n=int(input())
+# sum=0
+# i=0
+# a=0
+# while i<=n:
+#     a=a*10+5
+
+# while i<=len(a):
+#     j=0
+#     while j<=len(a):
+#             for i in a:
+#                 if i==4:
+#                 s=list(a[i])
+# #         for i in s:
+            #   if i==
+#             b.append(j)len(b)):
+                # print(a[i][j"],end=")
+            #             s=list(a[i])
+            #             for j in s:
+            
+#         else:
+#             b.append(a[i])
+# print(b)
+
+
+# a=[1,2,3,[4,5],6,[7,8],9]
+# d=1
+# for i in range(d):len(b)):
+                # print(a[i][j"],end=")
+            #             s=list(a[i])
+            #             for j in s:
+            
+            # b=[]
+            # for j in range(len(b)):
+                # print(a[i][j"],end=")
+            #             s=list(a[i])
+            #             for j in s:
+            #                b.append(j)
+            # else:
+                        
+            #           b.append(a[i]) 
+            # print()
+# print(b)      
+
+
+
+
+
+# i=0
+# while i<5:
+#     b=0
+#     while b<=5-i:
+#         print(" ",end="")
+#         b+=1
+#     j=1
+#     while j<=i:
+#         k=1
+#         print(k,end="") 
+#         k+=1
+#         j+=1
+#     print()    
+#     i+=1
+
+# s="my name is sangeeta and i live in dehli"
+# i=0
+# while i<len(s):
+
+# n=int(input())
+# unit=int(input("enter a unit"))
+# if n<=100:
+#    print("no charge")
+# elif n<=200:
+#       print((unit*5)-100)
+# elif n>200:
+#     print((unit*10)+(unit*5)-100)
+# else:
+#       ("please check the unit
+
+# a=int(input())
+# g=input()
+# b=int(input())
+# if g=="*":
+#     print(a*b)
+# elif g=="-":
+#     print(a-b)
+# elif g=="+":
+#     print(a+b)
+# elif g=="/":
+#     print(a/b)
+# else:
+#     print(a,b)
+
+
+
+
+
+
+
+# math=int(input("enterr "))
+# english=int(input("enter"))
+# computer=int(input("enter")) 
+# biology=int(input("enter"))
+# physics=int(input("enter"))
+# total=english+math+computer+biology+physics
+# per=total/500*100
+# print(per)
+
+# if per>=90:
+#   print("grade,a")
+# elif per>=80:
+#     print("grade,b")
+# elif per>=70:
+#     print("grade,c")
+# elif per>=60:
+#     print("grade,d")
+# elif per>=40:
+#     print("grade,e")
+# elif per<40:
+#     print("grade,f")
+# else:
+#      print("fail")   
+
+# que=dimond pattern  code?
+
+
+# i=1
+# while i<5:
+#     b=1
+#     while b<=5-i:
+#         print(" ",end="")
+#         b+=1
+#     j=0
+#     while j<=i+1:
+#         d=j-i
+#         if d==0 or d==1:
+#            pass
+#         else:
+#             print(i-j,end="")
+#         j+=1
+#     j=0
+#     while j<=i:
+#         if j==0 or j==1:
+#             pass
+#         else:
+#           print(j,end="")
+#         j+=1   
+#     print()    
+#     i+=1 
+# i=4
+# while i>1:
+#     b=0
+#     while b<=5-i:
+#         print(" ",end="")
+#         b+=1
+#     j=1
+#     while j<=i:
+#         if (i-j)==0:
+#             pass
+#         else:
+#           print(i-j,end="") 
+#         j+=1
+#     j=1
+#     while j<i:
+#         if j==1:
+#             pass
+#         else:
+#             print(j,end="")
+#         j+=1        
+#     i-=1
+#     print()            
+
+
+# a="hello world"
+# for i in a:
+#   if i==1:
+#     continue     
+    # print(a[i])
+# math=int(input("enterr "))
+# english=int(input("enter"))
+# computer=int(input("enter")) 
+# biology=int(input("enter"))
+# physics=int(input("enter"))
+# total=english+math+computer+biology+physics
+# per=total/500*100
+# print(per)
+
+# if per>=90:
+#   print("grade,a")
+# elif per>=80:
+#     print("grade,b")
+# elif per>=70:
+#     math=int(input("enterr "))
+# english=int(input("enter"))
+# computer=int(input("enter")) 
+# biology=int(input("enter"))
+# physics=int(input("enter"))
+# total=english+math+computer+biology+physics
+# per=total/500*100
+# print(per)
+
+# if per>=90:
+#   print("grade,a")
+# elif per>=80:
+#     print("grade,b")
+# elif per>=70:
+#     print("grade,c")
+# elif per>=60:
+#     print("grade,d")
+# elif per>=40:
+#     print("grade,e")
+# elif per<40:
+#     print("grade,f")
+# else:
+#      print("fail")   
+# print("grade,c")
+# elif per>=60:
+#     print("grade,d")
+# elif per>=40:
+#     print("grade,e")
+# elif per<40:
+#     print("grade,f")
+# else:
+#      print("fail"
+
+
+# a="ABCDE"
+# i=0
+# while i<len(a):
+#   b=1
+#   while b<=len(a)-i:
+#     print(" ",end="")
+#     b+=1
+#   j=0
+#   while j<i+1:
+#     print(a[-j-1],end="")
+    # j+=1
+#   j=0
+#   while j<i+1:
+#     if a[j]==a[i]:
+#         pass
+#     else:  
+#         print(a[-j],end="")
+#     j+=1  
+  # i+=1
+  # print()  
+# s=input("enter string")
+# vowel=0
+# for i in s:
+#   if i=='e' or i=='i' or i=='a' or i=='u'or i=='A' or i=='I' or i=='0' or i=='U':  
+#     print(i)
+# s='save,','Changes','In','The','Editor'
+# c=0
+# for i in s:
+#      c+=1
+# print(c)
+# s='Welcome to My Site https://nayaksworld.com '
+# print(s.find('come'))
+# print(s.find('o'))
+# print(s.find('o', 10, 20))
+# print(s.find('o', 5, 10)
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=int(input())    
+# if a<b:
+#   min=b
+#   max=a
+# else:
+#   min=a
+#   max=b
+# if c<d:
+#   min2=d
+#   max2=c
+# else:
+#   min2=c
+#   max2=d
+# if max<max2:
+#    yellow=max2
+# else:
+#     yellow=max
+# if min<min2:
+#    green=min2
+# else:
+#    green=min
+# if yellow<green:
+#    thirdmax=yellow
+# else:
+#    thirdmax=green
+# print(thirdmax)
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n=int(input())
+# i=1
+# while i<=n:
+#   if n%i==0:
+#     print(i)
+#   i+=1
+
+
+
+
+
+
+
+
+
+
+
